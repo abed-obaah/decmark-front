f53c4f4af1dcc0adeaa4f9bd8aca57aa85af7579
John Adibe <johnadibe450@gmail.com>
2022-04-28 22:01:12 +0100

    Setting up theming

---
+++ src/components/AppText.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components/native';
+
+export const Header = styled.Text``
+
+export const SubHeader = styled.Text``
+
+export const MediumText = styled.Text``
+
+export const SmallText = styled.Text``
--- src/constants/theme.js
+++ src/constants/theme.js
@@ -1,15 +1,17 @@
 export const COLORS = {
   primary: '#DEB253', // gold
   secondary: '#172B43', // blue
-  white: '#FFF',
-  dark: '#141414',
+  
   darkGold: "#825f15",
   light: '#E5E5E5',
   lighter: '#fafafa',
   grey: 'rgba(20, 20, 20, .5)',
   lightGrey: 'rgba(20, 20, 20, .35)',
   red: '#D91D13',
-  diabledBackground: 'rgba(222, 178, 83, .5)'
+  diabledBackground: 'rgba(222, 178, 83, .5)',
+
+  white: '#FFF',
+  dark: '#141414',
 }
 
 export const SIZES = {
